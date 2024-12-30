46bd8bba1ad467b3e6646fc0f007c7ad4cdc0647
areeba-awan <awanareeba40@gmail.com>
2024-12-30 23:52:10 +0500

    updated push code to github

--- src/app/Components/HomeContent.tsx
+++ src/app/Components/HomeContent.tsx
@@ -17,9 +17,9 @@ const HomeContent = () => {
         <h1 className="font-bold text-[32px] sm:text-[50px] md:text-[60px] lg:text-[70px] text-gray-200 leading-tight">
           Welcome to TRIPI World
         </h1>
-        <h2 className="font-mono text-[18px] sm:text-[22px] md:text-[28px] lg:text-[32px] text-red-50">
-        "Chase sunsets in lands unknown, where the sky kisses the horizon in shades of fiery gold."
-        </h2>
+        <p className="font-mono text-[18px] sm:text-[22px] md:text-[28px] lg:text-[32px] text-red-50">
+        Chase sunsets in lands unknown, where the sky kisses the horizon in shades of fiery gold.
+        </p>
         <Link
           href={"/Destinations"}
           className="bg-sky-500 hover:bg-green-900 text-white font-medium px-4 py-2 rounded-lg mt-4 text-[16px] sm:text-[18px]"
