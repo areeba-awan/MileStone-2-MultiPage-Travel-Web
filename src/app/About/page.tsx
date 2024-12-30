717f1ea0f1de17bb34e75540d1ded58e37bb3b42
areeba-awan <awanareeba40@gmail.com>
2024-12-31 00:10:49 +0500

    updated code push to github

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -14,13 +14,13 @@ const page = () => {
         />
 
         <div className="absolute inset-y-0 right-0 flex flex-col justify-center text-left bg-purple-300/80 px-6   py-8 sm:px-8 md:px-12 lg:px-16 w-full sm:w-[60%] md:w-[50%] lg:w-[40%]">
-          <h1 className="font-bold text-[32px] sm:text-[50px] md:text-[60px] lg:text-[70px] text-stone-700 font-serif mb-4 leading-tight">
+          <h1 className="font-bold text-[32px] sm:text-[50px] md:text-[60px] lg:text-[70px] text-stone-700 mb-4 leading-tight">
             About Us
           </h1>
-          <p className="font-sans text-[16px] sm:text-[18px] md:text-[20px] lg:text-[22px] text-zinc-700 leading-tight">&quot;
-            Welcome to TRIPI WORLD, We believe that every journey is more than just a destination,its an adventure that begins the moment you step out your door.
-            Imagine wandering through the vibrant souks of Marrakech, where the scent of spices fills the air, and the colors of intricate tapestries dazzle your senses. Or picture yourself on the pristine beaches of the Maldives, where turquoise waters and white sands create the perfect paradise. With Tripi, these exotic locations are within your reach, offering unique experiences that enrich your soul"
-            </p>
+          <p className="font-sans text-[16px] sm:text-[18px] md:text-[20px] lg:text-[22px] text-zinc-700 leading-tight">
+            Welcome to TRIPI WORLD, Travel is more than just the act of moving from one place to another; it is an enriching experience that broadens our horizons, introduces us to new cultures, and creates lifelong memories. Whether it's a weekend getaway or a month-long journey, the joys of travel are truly unparalleled.
+
+One of the most exciting aspects of travel is the opportunity to explore new destinations.</p>
         </div>
       </div>
     </div>
