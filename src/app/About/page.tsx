7e1052b85bd2ab5506995e0b7027e3f5aeef8583
areeba-awan <awanareeba40@gmail.com>
2024-12-30 23:58:45 +0500

    updated code push to github

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -17,10 +17,10 @@ const page = () => {
           <h1 className="font-bold text-[32px] sm:text-[50px] md:text-[60px] lg:text-[70px] text-stone-700 font-serif mb-4 leading-tight">
             About Us
           </h1>
-          <h2 className="font-sans text-[16px] sm:text-[18px] md:text-[20px] lg:text-[22px] text-zinc-700 leading-tight">
+          <p className="font-sans text-[16px] sm:text-[18px] md:text-[20px] lg:text-[22px] text-zinc-700 leading-tight">&quot;
             Welcome to TRIPI WORLD, We believe that every journey is more than just a destination,its an adventure that begins the moment you step out your door.
-            Imagine wandering through the vibrant souks of Marrakech, where the scent of spices fills the air, and the colors of intricate tapestries dazzle your senses. Or picture yourself on the pristine beaches of the Maldives, where turquoise waters and white sands create the perfect paradise. With Tripi, these exotic locations are within your reach, offering unique experiences that enrich your soul."
-          </h2>
+            Imagine wandering through the vibrant souks of Marrakech, where the scent of spices fills the air, and the colors of intricate tapestries dazzle your senses. Or picture yourself on the pristine beaches of the Maldives, where turquoise waters and white sands create the perfect paradise. With Tripi, these exotic locations are within your reach, offering unique experiences that enrich your soul"
+            </p>
         </div>
       </div>
     </div>
